f81fde0b7471328ef904b5cbf6b6ae4b79ce7a9a
baude <bbaude@redhat.com>
2018-03-12 15:29:04 -0500

    build1
    Signed-off-by: baude <bbaude@redhat.com>

--- buildah/build.sh
+++ buildah/build.sh
@@ -15,12 +15,14 @@ COMMIT=$(git rev-parse HEAD)
 COMMIT_SHORT=$(git rev-parse --short HEAD)
 COMMIT_NUM=$(git rev-list HEAD --count)
 COMMIT_DATE=$(date --date="@$(git show -s --format=%ct HEAD)" +%Y%m%d)
+TARBALL="${resultdir}/cni-${COMMIT_SHORT}.tar.gz"
 
-git archive --prefix "${package}-${COMMIT_SHORT}/" --format "tar.gz" HEAD -o "${resultdir}/${package}-${COMMIT_SHORT}.tar.gz"
+git archive --prefix "${package}-${COMMIT_SHORT}/" --format "tar.gz" HEAD -o "${TARBALL}"
 
 curl -o ${resultdir}/${package}.spec.in https://raw.githubusercontent.com/baude/copr/master/${package}/${package}.spec.in
 
 sed "s,#COMMIT#,${COMMIT},;
+     s,#SOURCE#,${TARBALL},;
      s,#SHORTCOMMIT#,${COMMIT_SHORT},;
      s,#COMMITNUM#,${COMMIT_NUM},;
      s,#COMMITDATE#,${COMMIT_DATE}," \
