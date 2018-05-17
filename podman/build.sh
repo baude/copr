37e73a5b4d5434f9ff0419790ee66565f50c3325
baude <bbaude@redhat.com>
2018-05-17 08:52:02 -0500

    podman
    Signed-off-by: baude <bbaude@redhat.com>

---
+++ podman/build.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+# https://docs.pagure.org/copr.copr/custom_source_method.html#custom-source-method
+
+set -x
+set -e
+
+mkdir -p results
+resultdir=$(readlink -f results)
+
+package=libpod
+clone_url=https://github.com/projectatomic/libpod
+
+git clone ${clone_url}
+cd ${package}
+
+COMMIT=$(git rev-parse HEAD)
+COMMIT_SHORT=$(git rev-parse --short HEAD)
+COMMIT_NUM=$(git rev-list HEAD --count)
+COMMIT_DATE=$(date --date="@$(git show -s --format=%ct HEAD)" +%Y%m%d)
+TARBALL="${resultdir}/cni-${COMMIT_SHORT}.tar.gz"
+
+git archive --prefix "${package}-${COMMIT_SHORT}/" --format "tar.gz" HEAD -o "${TARBALL}"
+
+curl -o ${resultdir}/${package}.spec.in https://raw.githubusercontent.com/baude/copr/master/${package}/${package}.spec.in
+
+sed "s,#COMMIT#,${COMMIT},;
+     s,#SOURCE#,${TARBALL},;
+     s,#SHORTCOMMIT#,${COMMIT_SHORT},;
+     s,#COMMITNUM#,${COMMIT_NUM},;
+     s,#COMMITDATE#,${COMMIT_DATE}," \
+         ${resultdir}/${package}.spec.in > ${resultdir}/${package}.spec
+
