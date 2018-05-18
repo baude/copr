555ebd18de81b7a270d71c939c104a850d70679b
baude <bbaude@redhat.com>
2018-05-18 09:25:56 -0500

    podman varlink python
    Signed-off-by: baude <bbaude@redhat.com>

--- podman/build.sh
+++ podman/build.sh
@@ -7,17 +7,17 @@ set -e
 mkdir -p results
 resultdir=$(readlink -f results)
 
-package=libpod
+package=podman
 clone_url=https://github.com/projectatomic/libpod
 
 git clone ${clone_url}
-cd ${package}
+cd libpod
 
 COMMIT=$(git rev-parse HEAD)
 COMMIT_SHORT=$(git rev-parse --short HEAD)
 COMMIT_NUM=$(git rev-list HEAD --count)
 COMMIT_DATE=$(date --date="@$(git show -s --format=%ct HEAD)" +%Y%m%d)
-TARBALL="${resultdir}/cni-${COMMIT_SHORT}.tar.gz"
+TARBALL="${resultdir}/podman-${COMMIT_SHORT}.tar.gz"
 
 git archive --prefix "${package}-${COMMIT_SHORT}/" --format "tar.gz" HEAD -o "${TARBALL}"
 
